ffffd079161336c37c2ce9889af17dee12f9e161
IrinaBarragan <80377933+IrinaBarragan@users.noreply.github.com>
2025-06-04 09:33:33 +0200

    fix port

--- server.py
+++ server.py
@@ -113,4 +113,5 @@ async def analyze_text(request: InputText):
     return analyze_prompt(request.text)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
